a2dcda44278b259a7308b65dea4584484301fc71
yooseungju <seung338989@gmail.com>
2019-03-13 07:06:53 +0000

    first commit

---
+++ boards/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = "boards"
+
+
+urlpatterns = [
+    path('<int:pk>/update/', views.update, name = 'update'),
+    path('<int:pk>/edit/', views.edit, name = 'edit'),
+    path('<int:pk>/delete/', views.delete, name = 'delete'),
+    path('<int:pk>/detail/', views.detail, name = 'detail'),
+    path('create/', views.create, name = 'create'),
+    path('new/', views.new, name = 'new'),
+    path('', views.index, name ='index'),
+]
+    
