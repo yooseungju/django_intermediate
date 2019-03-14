60baaf31ed9acc957208984480d7cb0ed42f6273
yooseungju <seung338989@gmail.com>
2019-03-14 07:55:25 +0000

    01_1:N model relation

--- boards/urls.py
+++ boards/urls.py
@@ -5,10 +5,12 @@ from . import views
 app_name = "boards"
 
 
-urlpatterns = [
-    path('<int:pk>/edit/', views.edit, name = 'edit'),
-    path('<int:pk>/delete/', views.delete, name = 'delete'),
-    path('<int:pk>/detail/', views.detail, name = 'detail'),
+urlpatterns = [   
+    path('<int:board_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name = 'comments_delete'),
+    path('<int:board_pk>/comments/', views.comments_create, name = 'comments_create'),
+    path('<int:board_pk>/edit/', views.edit, name = 'edit'),
+    path('<int:board_pk>/delete/', views.delete, name = 'delete'),
+    path('<int:board_pk>/detail/', views.detail, name = 'detail'),
     path('new/', views.new, name = 'new'),
     path('', views.index, name ='index'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Board
+from .models import Board, Comment
 
 
 # Create your views here.
@@ -26,43 +26,65 @@ def new(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
-        board = Board(title= title, content=content)
+        board = Board(title = title, content=content)
         board.save()
-        return redirect('boards:detail', pk=board.id)
+        return redirect('boards:detail',board.id)
     #new 일때
     else:
         return render(request, 'boards/new.html')
     
-def detail(request, pk):
-    board = Board.objects.get(pk=pk)
-    content = {
-        'board' : board
+def detail(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+    comments = board.comment_set.all()
+    context = {
+        'board' : board,
+        'comments': comments,
     }
-    return render(request, 'boards/detail.html', content)
+    return render(request, 'boards/detail.html', context )
     
-def delete(request, pk):
-    board = Board.objects.get(pk=pk)
+def delete(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
     if request.method == "POST":
         board.delete()
         return redirect('boards:index')
     else: 
         return redirect('boards:detail', board.pk)
     
-def edit(request, pk):
+def edit(request, board_pk):
     if request.method == "POST":
         title = request.POST.get('title')
         content = request.POST.get('content')
-        board = Board.objects.get(pk=pk)
+        board = Board.objects.get(pk=board_pk)
         board.title = title
         board.content = content
         board.save()
         return redirect('boards:detail', board.id)
     else:
-        board = Board.objects.get(pk = pk)
+        board = Board.objects.get(pk = board_pk)
         context ={
             'board' : board ,
         }
         return render(request, 'boards/edit.html', context)
+        
+def comments_create(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+    
+    # form 에서 넘어온 comment data
+    content = request.POST.get('content')
+    
+    # 댓글 생성 및 저장
+    comment = Comment(board=board, content=content)
+    comment.save()
+    return redirect('boards:detail', board.pk)
+    
+    
+def comments_delete(request, board_pk, comment_pk ):
+    comment = Comment.objects.get(pk = comment_pk)
+    if request.method ==  'POST':
+        comment.delete()
+        return redirect('boards:detail', board_pk)
+    else:
+        return redirect('boards:detail', board_pk)
     
 
     
